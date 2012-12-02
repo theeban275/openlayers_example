9ceef4439366c7c46eca850dbe54e600bc41ab9f
Theeban Soundararajan <theeban@intersect.org.au>
2012-12-03 10:40:06 +1100

    Fixing for use on mac

--- app/controllers/map_view_controller.rb
+++ app/controllers/map_view_controller.rb
@@ -13,9 +13,15 @@ class MapViewController < ApplicationController
     @feature = feature
     db = SQLite3::Database.new("#{Rails.root}/db/map.sqlite3")
     db.enable_load_extension(true)
-    db.execute("select load_extension('libspatialite.so');")
+    db.execute("select load_extension('#{get_extension}');")
     rows = db.execute("select astext(geometry) from pg_#{feature};")
     rows.map{ |x| x.first }
   end
+
+  private 
+    def get_extension
+      return 'libspatialite.dylib' if (/darwin/ =~ RUBY_PLATFORM) != nil
+      return 'libspatialite.so'
+    end
 
 end
